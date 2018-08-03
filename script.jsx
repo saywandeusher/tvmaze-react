dbc34d74a4c2077667e02b0e23e927c8ec249722
saywandeusher <ongsaywan@hotmail.com>
2018-08-03 18:24:58 +0800

    working on search component

--- script.jsx
+++ script.jsx
@@ -1,32 +1,6 @@
-class Home extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-
-      };
-    }
-
+class Result extends React.Component {
   render() {
-    return (
-      <div className="home">
-        <h1>TVMaze React</h1>
-        <div className="search">
-          <SearchBar />
-        </div>
-      </div>
-    );
-  }
-}
 
-class SearchBar extends React.Component {
-  render() {
-    return (
-      <form>
-        <input type="text" placeholder="Enter a Movie Title..." />
-        <br />
-        <button type="submit">search</button>
-      </form>
-    );
   }
 }
 
@@ -36,9 +10,63 @@ class ResultsTable extends React.Component {
   }
 } 
 
-class Result extends React.Component {
-  render() {
+class SearchBar extends React.Component {
+  constructor(props){
+    super(props)
+    this.changeHandler = this.changeHandler.bind(this);
+    this.handleClick = this.handleClick.bind( this );
+    this.state = {
+      input : "",
+      results : false
+    }
+  }
 
+  inputHandler(event){
+    let inputValue = event.target.value;
+    this.setState({input:inputValue});
+    console.log(this.state.value);
+  }
+
+  handleClick(){
+    let inputValue = this.state.input;
+    if (inputValue.length < 1) {
+      alert("Please enter a task!");
+    } else {
+      this.setState({input: inputValue, results: true})
+    }
+  }
+
+  render() {
+    return (
+      <form>
+        <input onChange={this.inputHandler} value={this.state.input} placeholder="Enter a Movie Title..." />
+        <br />
+        <button type="button" onClick={this.props.click}>search</button>
+      </form>
+    );
+  }
+}
+
+class Home extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      test: true
+    }
+  }
+  changeTest = () => {
+    this.setState({test:!this.state.test}, ()=>{console.log(this.state.test)});
+  }
+  render() {
+    const condition = true;
+    return (
+      <div className="home">
+        <h1>TVMaze React</h1>
+        <div className="search">
+          <SearchBar click={this.changeTest}/>
+        </div>
+      </div>
+    );
   }
 }
 
