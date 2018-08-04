458ada36c5f542f31864f6396526a8dc04091c62
saywandeusher <ongsaywan@hotmail.com>
2018-08-04 11:28:04 +0800

    working on input onSubmit

--- script.jsx
+++ script.jsx
@@ -6,15 +6,19 @@ class Result extends React.Component {
 
 class ResultsTable extends React.Component {
   render() {
-
+    return(
+      <div className="resultsTable">
+        <h1>Your results!</h1>
+      </div>
+    );
   }
 } 
 
 class SearchBar extends React.Component {
   constructor(props){
     super(props)
-    this.changeHandler = this.changeHandler.bind(this);
-    this.handleClick = this.handleClick.bind( this );
+    this.inputHandler = this.inputHandler.bind(this);
+    this.handleClick = this.handleClick.bind(this);
     this.state = {
       input : "",
       results : false
@@ -24,47 +28,57 @@ class SearchBar extends React.Component {
   inputHandler(event){
     let inputValue = event.target.value;
     this.setState({input:inputValue});
-    console.log(this.state.value);
+    console.log(inputValue);
   }
 
   handleClick(){
     let inputValue = this.state.input;
     if (inputValue.length < 1) {
       alert("Please enter a task!");
+      return;
     } else {
-      this.setState({input: inputValue, results: true})
+      this.setState({input: inputValue, results: true}, ()=>{console.log(this.state)});
     }
   }
 
   render() {
+    let showResults;
+    if (this.state.results) {
+      showResults = <ResultsTable />
+    }else{
+      showResults = <div><p>Nothing was searched.</p></div>
+    }
+
     return (
-      <form>
-        <input onChange={this.inputHandler} value={this.state.input} placeholder="Enter a Movie Title..." />
-        <br />
-        <button type="button" onClick={this.props.click}>search</button>
-      </form>
+      <div className="search">
+        <form>
+          <input onChange={this.inputHandler} value={this.state.input} onSubmit={this.handleClick} placeholder="Enter a Movie Title..." />
+          <br />
+          <button type="button" onClick={this.handleClick}>search</button>
+        </form>
+        {showResults}
+      </div>
     );
   }
+
 }
 
 class Home extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={
-      test: true
-    }
-  }
-  changeTest = () => {
-    this.setState({test:!this.state.test}, ()=>{console.log(this.state.test)});
-  }
+  // constructor(props){
+  //   super(props);
+  //   this.state={
+  //     test: true
+  //   }
+  // }
+  // changeTest = () => {
+  //   this.setState({test:!this.state.test}, ()=>{console.log(this.state.test)});
+  // }
   render() {
-    const condition = true;
+    // const condition = true;
     return (
       <div className="home">
         <h1>TVMaze React</h1>
-        <div className="search">
-          <SearchBar click={this.changeTest}/>
-        </div>
+          <SearchBar /*click={this.changeTest}*/ />
       </div>
     );
   }
